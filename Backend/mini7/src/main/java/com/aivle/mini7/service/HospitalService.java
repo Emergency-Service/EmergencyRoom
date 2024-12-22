12e07ca07e422b76445fb59de47dedda118ffc96
gh <dinggul56@gmail.com>
2024-12-22 16:16:52 +0900

    feat: fastApiClient를 구현하는 HospitalService 클래스 작성
    - Feign 클라이언트 라이브러리는 인터페이스를 자동으로 구현하기 때문에 의존성 주입 받아 리턴만 함

--- Backend/mini7/src/main/java/com/aivle/mini7/service/HospitalService.java
+++ Backend/mini7/src/main/java/com/aivle/mini7/service/HospitalService.java
@@ -1,4 +1,19 @@
 package com.aivle.mini7.service;
 
+import org.springframework.stereotype.Service;
+
+import com.aivle.mini7.client.api.FastApiClient;
+import com.aivle.mini7.client.dto.HospitalResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class HospitalService {
+
+    private final FastApiClient fastApiClient;
+
+    public HospitalResponse getNearestHospitals(String request, double latitude, double longitude, int count){
+        return fastApiClient.getNearestHospitals(request, latitude, longitude, count);
+    }
 }
